528aff22fa2ae3c0cb3ded60a30ea6c6a0745152
Thiru-maran <131544104+Thiru-maran@users.noreply.github.com>
2024-02-11 19:18:54 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+/*
+  cloud {
+    organization = "KynMig1"
+
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+*/
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
